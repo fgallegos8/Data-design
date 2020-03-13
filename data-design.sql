f9290e8d5021e0907d29e6e0d4a346a2fb27991b
Francisco Gallegos <fgallegos59@cnm.edu>
2020-03-13 11:44:54 -0600

    Used html to create a persona profile

--- data-design.sql
+++ data-design.sql
@@ -1,1 +1,38 @@
 use fgallegos59;
+ALTER DATABASE fgallegos59 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS articleTag;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS author;
+
+CREATE TABLE user(
+     userId BINARY(16) NOT NULL,
+     userEmail VARCHAR(128) NOT NULL,
+     userHash CHAR(97) NOT NULL,
+     userName VARCHAR(32) NOT NULL,
+     userActivationToken CHAR (32),
+     UNIQUE(userEmail),
+     UNIQUE(userName),
+     INDEX(userEmail),
+     PRIMARY KEY(userId)
+);
+
+CREATE TABLE feedback(
+    feedbackId BINARY (16) NOT NULL,
+    feedbackUserId BINARY (16) NOT NULL,
+    feedbackContentId BINARY (16) NOT NULL,
+    PRIMARY KEY (feedbackId),
+    FOREIGN KEY (feedbackUserId) REFERENCES user(userID) ,
+    FOREIGN KEY (feedbackContentId) REFERENCES content(contentId)
+);
+
+CREATE TABLE content(
+    contentId BINARY(16) NOT NULL,
+    contentGenre VARCHAR(128) NOT NULL,
+    contentName VARCHAR(32) NOT NULL,
+    UNIQUE (contentId),
+    UNIQUE (contentName),
+    INDEX (contentName),
+    PRIMARY KEY (contentId)
+);
